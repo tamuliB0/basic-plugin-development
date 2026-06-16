b884f8fb9b95885d6e8db29124bc91a64c83e623
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-06-16 17:50:05 +0530

    add plugin header to list plugin in Wordpress plugin dashboard

---
+++ wp-content/plugins/wp-book/wp-book.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WP Book
+ * Description: A WordPress plugin for managing books using custom post types and taxonomies.
+ * Version: 1.0.0
+ * Author: tamuliB0
+ * 
+ * @package wp-book
+ */
