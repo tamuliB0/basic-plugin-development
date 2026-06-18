ce5235fa2c6fa58b6bc1944a8a53ddb521cc70c3
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-06-18 15:00:28 +0530

    add Settings submenu under Book post type menu

---
+++ wp-content/plugins/wp-book/includes/admin-page.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Register admin submenu page under Books.
+ */
+function wp_book_register_admin_page() {
+	add_submenu_page(
+		'edit.php?post_type=book',  // slug (parent-slug) for Custom Post Types: ‘edit.php?post_type=your_post_type’ 
+		__( 'Book Settings', 'wp-book' ), 
+		__( 'Settings', 'wp-book' ), 
+		'manage_options', 
+		'wp_book_settings', 
+		'wp_book_settings_page_html' 
+	);
+}
+add_action( 'admin_menu', 'wp_book_register_admin_page' );
--- wp-content/plugins/wp-book/wp-book.php
+++ wp-content/plugins/wp-book/wp-book.php
@@ -17,5 +17,6 @@ require_once $dir . 'includes/taxonomies.php';
 require_once $dir . 'includes/db.php';
 require_once $dir . 'includes/meta-box.php';
 require_once $dir . 'includes/save-meta.php';
+require_once $dir . 'includes/admin-page.php';
 
 register_activation_hook( __FILE__, 'wp_book_create_meta_table' );
