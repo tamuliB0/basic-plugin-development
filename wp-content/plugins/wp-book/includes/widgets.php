6bf801d231f6d367b1b7db852e2cc0eaa93ce2c4
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-06-19 16:51:37 +0530

    add initial code structure for book category widget

--- wp-content/plugins/wp-book/includes/widgets.php
+++ wp-content/plugins/wp-book/includes/widgets.php
@@ -50,3 +50,66 @@ function wp_book_register_sidebar() {
     );
 }
 add_action( 'widgets_init', 'wp_book_register_sidebar' );
+
+/**
+ * Widget to display books from a selected book category.
+ */
+class WP_Book_Category_Widget extends WP_Widget {
+	public function __construct() {
+		parent::__construct(
+			'wp_book_category_widget',
+			__( 'WP Book Category Widget', 'wp-book' ),
+			array(
+				'description' => __( 'Displays books from a selected book category.', 'wp-book' ),
+            )
+        );
+	}
+	public $args = array(
+		'before_title'  => '<h4 class="widgettitle">',
+		'after_title'   => '</h4>',
+		'before_widget' => '<div class="widget-wrap">',
+		'after_widget'  => '</div></div>',
+	);
+
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+        $category_id = ! empty( $instance['category_id'] ) ? absint( $instance['category_id'] ) : 0;
+        if ( $category_id ) {
+			$book_query = new WP_Query(
+				array(
+					'post_type'      => 'book',
+					'posts_per_page' => 5,
+					'tax_query'      => array(
+						array(
+							'taxonomy' => 'book-category',
+							'field'    => 'term_id',
+							'terms'    => $category_id,
+						),
+					),
+				)
+			);
+            if ( $book_query->have_posts() ) {
+                echo '<ul>';
+                while ( $book_query->have_posts() ) {
+                    $book_query->have_posts();
+                    echo '<li><a href="' . esc_url( get_permalink() ) . '">' . esc_html( get_the_title() ) . '</a></li>';
+                }
+                echo '</ul>';
+                wp_reset_postdata();
+            } else {
+                echo '<p>' . esc_html( 'No books found in this category.', 'wp-book' ) . '</p>';
+            }
+        } else {
+                echo '<p>' . esc_html( 'Please select a category.', 'wp-book' ) . '</p>';
+        }
+		echo $args['after_widget'];
+	}
+}
+
+/**
+ * Register WP Book category widget.
+ */
+function wp_book_register_widgets() {
+	register_widget( 'WP_Book_Category_Widget' );
+}
+add_action( 'widgets_init', 'wp_book_register_widgets' );
